48774b48fb7939fab7d09a0139eefd2b412947e8
Grant <Grantemc2@att.net>
2020-04-26 11:35:04 -0500

    basic search complete

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using CowboyCafe.Data;
 
 namespace Website.Pages
 {
@@ -17,9 +18,21 @@ namespace Website.Pages
             _logger = logger;
         }
 
+        /// <summary>
+        /// The current search terms 
+        /// </summary>
+        [BindProperty]
+        public string SearchTerms { get; set; } = "";
+
+        /// <summary>
+        /// The menu items to display on the index page 
+        /// </summary>
+        public IEnumerable<IOrderItem> MenuList { get; protected set; }
+
         public void OnGet()
         {
-
+            SearchTerms = Request.Query["SearchTerms"];
+            MenuList = Menu.Search(SearchTerms);
         }
     }
 }
